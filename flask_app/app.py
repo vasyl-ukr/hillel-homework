056a3e33ab4162233a8c9bafddb709df2e17cf50
vasyl-ukr <bandrivsky@gmail.com>
2022-06-27 12:22:35 +0300

    dz4 all done

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,44 +6,45 @@ from flask import Flask, request
 
 
 app = Flask(__name__)
-app.debug = True
 
 @app.route("/whoami/")
 def whoami():
-    user_agent = request.headers.get('User-Agent')
+    user_agent = request.headers.get('User-Agent', '')
     ip_address = request.remote_addr
     server_time = time.strftime('%A %B, %d %Y %H:%M:%S')
 
     return f"""
-    <h3/> User browser: {user_agent} </h3> 
-    <h3/> IP_address: {ip_address} </h3>
-    <h3/> Server time: {server_time} </h3>
+    <h4/> User browser: {user_agent} </h4> 
+    <h4/> IP_address: {ip_address} </h4>
+    <h4/> Server time: {server_time} </h4>
     """
 
 @app.route("/source_code/")
-def source_cod():
-    pass
+def source_code():
+    with open("app.py", "r") as f:
+        result = f.read()
+    return result
 
 @app.route("/random/")
-def home():
+def random_string():
 
-    length = int(request.values.get('length', ''))
+    length = request.values.get('length', '')
     specials = request.values.get('specials', '')
     digits = request.values.get('digits', '')
 
-    letters = string.ascii_letters
+    check_length = [str(i) for i in range(1, 101)]
+    check_bool = ['', '0', '1']
+
+    if not (length in check_length and specials in check_bool and digits in check_bool):
+        return "Not valid request"
+
+    symbols = string.ascii_letters
 
     if specials == "1":
-        letters += '!"№;%:?*()_+.'
+        symbols += '!"№;%:?*()_+.'
     if digits == "1":
-        letters += string.digits
+        symbols += string.digits
 
-    ans_str = ''.join(random.choice(letters) for i in range(length))
+    ans_str = ''.join(random.choice(symbols) for i in range(int(length)))
 
     return ans_str
-
-if __name__ == 'main':
-    app.run(use_reloader=True)
-
-
-
